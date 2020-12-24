1178d627b85a4b890cb76f9cf182d6128cf33975
ms250216 <muhammadusman.syed@teradata.com>
2020-12-24 14:09:31 +0500

    adding model

--- model_definitions/70a7ee68-2884-48ff-829d-47a43ac07d43/model_modules/training.py
+++ model_definitions/70a7ee68-2884-48ff-829d-47a43ac07d43/model_modules/training.py
@@ -1,7 +1,7 @@
 import pandas as pd
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.externals import joblib
-
+import seaborn as sns
 
 def train(data_conf, model_conf, **kwargs):
     """Python train method called by AOA framework
@@ -18,16 +18,17 @@ def train(data_conf, model_conf, **kwargs):
     hyperparams = model_conf["hyperParameters"]
 
     # load data & engineer
-    iris_df = pd.read_csv(data_conf['location'])
-    features = 'sepallength,sepalwidth,petallength,petalwidth'.split(',')
-    X = iris_df.loc[:, features]
-    y = iris_df['class']
+    walmart_df = pd.read_csv(data_conf['data_set_1'])
+    print(walmart_df.head())
+    # features = 'sepallength,sepalwidth,petallength,petalwidth'.split(',')
+    # X = iris_df.loc[:, features]
+    # y = iris_df['class']
 
-    print("Starting training...")
-    # fit model to training data
-    knn = KNeighborsClassifier(n_neighbors=hyperparams['n_neighbors'])
-    knn.fit(X,y)
-    print("Finished training")
+    # print("Starting training...")
+    # # fit model to training data
+    # knn = KNeighborsClassifier(n_neighbors=hyperparams['n_neighbors'])
+    # knn.fit(X,y)
+    # print("Finished training")
 
-    joblib.dump(knn, 'artifacts/output/iris_knn.joblib')
-    print("Saved trained model")
+    # joblib.dump(knn, 'artifacts/output/iris_knn.joblib')
+    # print("Saved trained model")
